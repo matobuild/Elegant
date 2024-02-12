5186625f6cc468c21d3d3cb41ddd4cfa8aeae996
Mato <Kittawat@icloud.com>
2024-02-12 15:46:01 +0800

    apply morgan to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
+
 const PORT = process.env.PORT || 3000
 
 // Middleware to parse JSON bodies
 app.use(express.json())
+app.use(morgan("dev"))
 
 // Define a route handler for the root URL
 app.get("/", (req, res) => {
