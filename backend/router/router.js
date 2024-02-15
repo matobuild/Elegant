bbc5df0d861f7818e438aff548ff7c57833ec2cf
Mato <Kittawat@icloud.com>
2024-02-15 22:04:43 +0800

    install axios, tailwind and prettier-plugin-tailwindcss for frontend

--- backend/router/router.js
+++ backend/router/router.js
@@ -3,7 +3,7 @@ const userService = require("../service/userService")
 
 const router = express.Router()
 
-router.route("/users").post(userService.signUp)
+router.route("/users/").post(userService.signUp)
 router.route("/users/login").post(userService.signIn)
 
 module.exports = router
