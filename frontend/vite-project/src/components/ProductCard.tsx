a04a737f9cec937b81b052a66f5670751b843d38
Mato <Kittawat@icloud.com>
2024-02-26 00:50:25 +0800

    Update ProductCard component and utils

--- frontend/vite-project/src/components/ProductCard.tsx
+++ frontend/vite-project/src/components/ProductCard.tsx
@@ -1,10 +1,45 @@
 import star from "../../assets/icons/star-icon.svg"
-import { getImageUrl } from "../utils/utils"
+import { getImageFromData } from "../utils/utils"
 
 const ProductCard = () => {
   return (
-    <div className="flex flex-col gap-3">
-      <img src={getImageUrl("Home/sofa.png")} alt="" />
+    <div className=" flex max-h-[433px] w-full max-w-[262px] flex-col gap-3">
+      <div className="relative w-full">
+        <img
+          className="h-auto w-full"
+          src={getImageFromData("products/Bamboo Basket.png")}
+          alt="Bamboo-Basket"
+        />
+        <div className=" absolute left-4 top-4 inline-flex flex-col gap-2">
+          <div className="hairline-1  bg-neutral-1 text-neutral-7  flex content-center items-center rounded px-[14px] py-1 text-center">
+            NEW
+          </div>
+          <div className=" bg-secondary-green text-neutral-1 hairline-1 flex content-center items-center rounded px-[14px] py-1 ">
+            -50%
+          </div>
+        </div>
+        <div className="bg-neutral-1 absolute right-4 top-4 rounded-[32px] p-[6px] shadow-[0_8px_16px_-8px_rgba(15,15,15,0.12)]">
+          <svg
+            className="flex-1 self-stretch"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.577 5.76422C10.2546 6.07365 9.74545 6.07365 9.42301 5.76422L8.84601 5.2105C8.17065 4.56239 7.25829 4.16667 6.25001 4.16667C4.17894 4.16667 2.50001 5.8456 2.50001 7.91667C2.50001 9.90219 3.57483 11.5417 5.12647 12.8888C6.67944 14.237 8.53618 15.1312 9.64555 15.5876C9.87751 15.683 10.1225 15.683 10.3545 15.5876C11.4638 15.1312 13.3206 14.237 14.8735 12.8888C16.4252 11.5417 17.5 9.90218 17.5 7.91667C17.5 5.8456 15.8211 4.16667 13.75 4.16667C12.7417 4.16667 11.8294 4.56239 11.154 5.2105L10.577 5.76422ZM10 4.00798C9.02676 3.074 7.70542 2.5 6.25001 2.5C3.25847 2.5 0.833344 4.92512 0.833344 7.91667C0.833344 13.2235 6.64196 16.1542 9.0115 17.1289C9.64965 17.3914 10.3504 17.3914 10.9885 17.1289C13.3581 16.1542 19.1667 13.2235 19.1667 7.91667C19.1667 4.92512 16.7416 2.5 13.75 2.5C12.2946 2.5 10.9733 3.074 10 4.00798Z"
+              fill="#6C7275"
+            />
+          </svg>
+        </div>
+        <button className=" bg-neutral-7 text-neutral-1 button-s absolute bottom-4 left-4 right-4 rounded-lg px-6 py-2 text-center shadow-[0_8px_16px_0px_rgba(0,0,0,0.04)]">
+          Add to Car
+        </button>
+      </div>
+
       <div className="flex flex-col">
         <div className="g-[2px] flex">
           <img src={star} alt="start-icon" />
@@ -12,7 +47,13 @@ const ProductCard = () => {
           <img src={star} alt="start-icon" />
           <img src={star} alt="start-icon" />
         </div>
-        {/* NEED TO FINISH THE REST */}
+        <p className=" text-neutral-7 body-2-semi self-stretch">
+          Loveseat Sofa
+        </p>
+        <div className="flex gap-3">
+          <p className=" caption-1-semi text-neutral-7">$199.00</p>
+          <p className=" text-neutral-4 caption-1 line-through">$400.00</p>
+        </div>
       </div>
     </div>
   )
--- frontend/vite-project/src/utils/utils.ts
+++ frontend/vite-project/src/utils/utils.ts
@@ -1,3 +1,7 @@
 export const getImageUrl = (path: unknown) => {
   return new URL(`/assets/${path}`, import.meta.url).href
 }
+
+export const getImageFromData = (path: unknown) => {
+  return new URL(`../../../../backend2/assets/${path}`, import.meta.url).href
+}
