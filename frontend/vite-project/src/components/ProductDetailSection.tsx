2964f99633a80e4b583db0a44b9bc85d1d5b171f
kittawat phuangsombat (mato) <51300404+matobuild@users.noreply.github.com>
2024-03-05 16:07:26 +0800

    added the product detailpage

---
+++ frontend/vite-project/src/components/ProductDetailSection.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+
+const ProductDetailSection = () => {
+  return (
+    <section>
+      <div className="py-4">
+        <div className="px-40">
+          <div className="flex gx-4">
+            <button className="gx-1">
+              <span>Home</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M4.58423 3L7.58423 6L4.58423 9"
+                    stroke="#605F5F"
+                    stroke-width="0.75"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+            </button>
+            <button className="gx-1">
+              <span>Shop</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M4.58423 3L7.58423 6L4.58423 9"
+                    stroke="#605F5F"
+                    stroke-width="0.75"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+            </button>
+            <button className="gx-1">
+              <span>Living Room</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M4.58423 3L7.58423 6L4.58423 9"
+                    stroke="#605F5F"
+                    stroke-width="0.75"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+            </button>
+            <button className="">
+              <span>Product</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="px-40 flex gx-[63px]">
+        <div className="flex flex-col"></div>
+      </div>
+    </section>
+  )
+}
+export default ProductDetailSection
