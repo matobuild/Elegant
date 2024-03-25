cb2b070a996b73777c50f8297464c2016bfd08ff
Mato <Kittawat@icloud.com>
2024-03-25 23:48:39 +0800

    product card able to click add to wishlist

--- frontend/vite-project/src/components/MyAccount/WishlistTab.tsx
+++ frontend/vite-project/src/components/MyAccount/WishlistTab.tsx
@@ -1,5 +1,56 @@
+import { getImageFromData } from "../../utils/utils"
+
 const WishlistTab = () => {
-  return <div>WishlistTab</div>
+  return (
+    <div className=" px-[72px]">
+      <div className=" flex flex-col gap-10 ">
+        <p className="text-primary-1 body-1-semi">Your Wishlist</p>
+        <div className=" flex flex-col">
+          <div className=" border-neutral-3 caption-1 text-neutral-4 flex items-center justify-around border-b border-solid pb-2">
+            <p className=" w-1/3 pl-8">Product</p>
+            <p className=" w-1/3">Price</p>
+            <p className=" w-1/3">Action</p>
+          </div>
+          <div className=" border-neutral-3 flex items-center justify-around border-b border-solid py-6  ">
+            <div className=" w-1/3">
+              <div className="g-10px flex items-center ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M5.29289 5.79289C5.68342 5.40237 6.31658 5.40237 6.70711 5.79289L12 11.0858L17.2929 5.79289C17.6834 5.40237 18.3166 5.40237 18.7071 5.79289C19.0976 6.18342 19.0976 6.81658 18.7071 7.20711L13.4142 12.5L18.7071 17.7929C19.0976 18.1834 19.0976 18.8166 18.7071 19.2071C18.3166 19.5976 17.6834 19.5976 17.2929 19.2071L12 13.9142L6.70711 19.2071C6.31658 19.5976 5.68342 19.5976 5.29289 19.2071C4.90237 18.8166 4.90237 18.1834 5.29289 17.7929L10.5858 12.5L5.29289 7.20711C4.90237 6.81658 4.90237 6.18342 5.29289 5.79289Z"
+                    fill="#6C7275"
+                  />
+                </svg>
+                <div className=" flex gap-4">
+                  <div className=" h-[72px] w-[60px]">
+                    {/* <img
+                      className=" h-24 w-20 flex-shrink-0"
+                      src={getImageFromData(`${item.image_url}`)}
+                      alt=""
+                    /> */}
+                  </div>
+                  <p className=" caption-1-semi text-neutral-7">Tray Table</p>
+                </div>
+              </div>
+            </div>
+            <div className=" w-1/3">$19.19</div>
+            <div className=" w-1/3">
+              <button className="bg-neutral-7  rounded-lg px-6 py-[6px] text-white">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default WishlistTab
--- frontend/vite-project/src/components/ProductCard.tsx
+++ frontend/vite-project/src/components/ProductCard.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 import { USDollar, getImageFromData } from "../utils/utils"
 import StarsReview from "./StarsReview"
-import BlackButton from "./BlackButton"
 import usePostCheckoutCart from "../customHooks/usePostCheckoutCart"
+import { useState } from "react"
 
 type ProductCardProps = {
   img: string
@@ -23,6 +23,8 @@ const ProductCard = ({
   stars,
   id,
 }: ProductCardProps) => {
+  const [wishItem, setWishItem] = useState(false)
+
   const { postToCheckoutCart } = usePostCheckoutCart({
     product_id: id,
     quantity: 1,
@@ -36,6 +38,11 @@ const ProductCard = ({
     postToCheckoutCart()
   }
 
+  function handleWishItem(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.stopPropagation()
+    e.preventDefault()
+    setWishItem(!wishItem)
+  }
   return (
     <Link to={`/product/${title}`} state={{ id: id }}>
       <div className=" hover:border-neutral-3 group  flex w-full cursor-pointer flex-col gap-3 border border-transparent">
@@ -53,7 +60,13 @@ const ProductCard = ({
               -{discount}%
             </div>
           </div>
-          <div className="bg-neutral-1 absolute right-4 top-4 hidden rounded-[32px] p-[6px] shadow-[0_8px_16px_-8px_rgba(15,15,15,0.12)] group-hover:block">
+          <button
+            type="submit"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+              handleWishItem(e)
+            }}
+            className="bg-neutral-1 hover:bg-neutral-2 absolute right-4 top-4 hidden rounded-[32px] p-[6px] shadow-[0_8px_16px_-8px_rgba(15,15,15,0.12)] group-hover:block"
+          >
             <svg
               className="flex-1 self-stretch"
               xmlns="http://www.w3.org/2000/svg"
@@ -66,10 +79,11 @@ const ProductCard = ({
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M10.577 5.76422C10.2546 6.07365 9.74545 6.07365 9.42301 5.76422L8.84601 5.2105C8.17065 4.56239 7.25829 4.16667 6.25001 4.16667C4.17894 4.16667 2.50001 5.8456 2.50001 7.91667C2.50001 9.90219 3.57483 11.5417 5.12647 12.8888C6.67944 14.237 8.53618 15.1312 9.64555 15.5876C9.87751 15.683 10.1225 15.683 10.3545 15.5876C11.4638 15.1312 13.3206 14.237 14.8735 12.8888C16.4252 11.5417 17.5 9.90218 17.5 7.91667C17.5 5.8456 15.8211 4.16667 13.75 4.16667C12.7417 4.16667 11.8294 4.56239 11.154 5.2105L10.577 5.76422ZM10 4.00798C9.02676 3.074 7.70542 2.5 6.25001 2.5C3.25847 2.5 0.833344 4.92512 0.833344 7.91667C0.833344 13.2235 6.64196 16.1542 9.0115 17.1289C9.64965 17.3914 10.3504 17.3914 10.9885 17.1289C13.3581 16.1542 19.1667 13.2235 19.1667 7.91667C19.1667 4.92512 16.7416 2.5 13.75 2.5C12.2946 2.5 10.9733 3.074 10 4.00798Z"
-                fill="#6C7275"
+                // fill="#6C7275"
+                fill={wishItem ? "#FF5630" : "#6C7275"}
               />
             </svg>
-          </div>
+          </button>
           <button
             className={
               "bg-neutral-7 text-neutral-1 button-s hover:bg-neutral-5 absolute bottom-4 left-4 right-4 hidden rounded-lg px-6 py-2 text-center shadow-[0_8px_16px_0px_rgba(0,0,0,0.04)] group-hover:block"
